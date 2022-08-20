4ec4d9109eedc9f2fdfdab7e70ad00a0f0ad4472
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-08-20 16:41:37 +0530

    Testing tailwind and global.css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import moduleCSS from "../styles/index.module.css";
 
 export default function Home() {
     return (
@@ -12,6 +11,8 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </div>
     );
 }
