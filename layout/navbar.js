91074a09ec4cadcbc3662298695167491d182212
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-09-02 15:51:42 +0530

    Adding navbar

--- layout/navbar.js
+++ layout/navbar.js
@@ -1,3 +1,31 @@
-export default function Navbar() {
-    return <div></div>;
+import "./navbar.css";
+import { ReactComponent as Logo } from "../../../components/micro/rocket.svg";
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="brand inline-ele">
+                <div className="div-logo inline-ele">
+                    <Logo className="logo" />
+                </div>
+            </div>
+            <ol className="menu inline-ele">
+                <li className="nav-hover">
+                    <a href="#aboutme">About</a>
+                </li>
+                <li className="nav-hover">
+                    <a href="#pastexperience">Experience</a>
+                </li>
+                <li className="nav-hover">
+                    <a href="#highlights">Work</a>
+                </li>
+                <li className="nav-hover">
+                    <a href="#contactme">Contact</a>
+                </li>
+                <li id="resume">Resume</li>
+            </ol>
+        </div>
+    );
 }
+export default Navbar;
