0723cf4e40550d028c70232bfbec972ead4b3602
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-09-08 21:22:34 +0530

    Adding tailwind

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
     return (
-        <div>
+        <div className="bg-black">
             <Head>
                 <title>Aryan Rajoria</title>
                 <meta
@@ -12,7 +12,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-bold underline text-slate-600">Hello world!</h1>
         </div>
     );
 }
